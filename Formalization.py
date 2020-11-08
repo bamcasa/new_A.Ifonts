c4921b94763571855e28281be9157693ffac450d
bamcasa <combeesang@gmail.com>
2020-11-08 15:20:12 +0900

    이미지 데이터 정형화

--- Formalization.py
+++ Formalization.py
@@ -1,22 +1,23 @@
 import os
 from PIL import Image, ImageOps
 import numpy as np
+import time
 
-inpput_directory = "Classified_image"
+input_directory = "Classified_image"
 output_directory = "Formalization_image"
-path, dirs, files = next(os.walk(f"{inpput_directory}"))
+path, dirs, files = next(os.walk(f"{input_directory}"))
 
 if not os.path.isdir(f"{output_directory}"):
     os.mkdir(f"{output_directory}")
 
 for dir in dirs:
-    path, dirs2, files = next(os.walk(f"{inpput_directory}/{dir}"))
+    path, dirs2, files = next(os.walk(f"{input_directory}/{dir}"))
 
     for file in files:
-        image = Image.open(f"{inpput_directory}/{dir}/{file}")
+        image = Image.open(f"{input_directory}/{dir}/{file}")
         x, y = image.size
 
-        if x > y :
+        if x >= y :
             new_size = x
             x_offset = 0
             y_offset = int((x-y)/2)
@@ -29,7 +30,7 @@ for dir in dirs:
         new_image = Image.new("RGBA", (new_size, new_size), background_color)
         new_image.paste(image, (x_offset, y_offset))
 
-        new_image = ImageOps.fit(new_image, (214,214), Image.ANTIALIAS)
+        new_image = ImageOps.fit(new_image, (204,204), Image.ANTIALIAS)
 
         base_image = Image.new("RGBA", (224, 224), background_color)
         base_image.paste(new_image, (10, 10))
@@ -39,5 +40,6 @@ for dir in dirs:
         if not os.path.isdir(f"{output_directory}/{dir}"):
             os.mkdir(f"{output_directory}/{dir}")
 
+        print(f"{output_directory}/{dir}/{file}")
         new_image.save(f"{output_directory}/{dir}/{file}")
 
