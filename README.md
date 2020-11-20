78ca85a1ae09af271df74d36cca250c9128645c6
Bokwang0310 <chdl2353@naver.com>
2020-11-21 00:05:21 +0900

    Separate dual && Update README.md 이중모음 만드는 걸 dual.py로 뺌

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # new_A.Ifonts
 
 영어는 대소문자를 모두 합쳐도 글자가 52자 밖에 되지 않아 폰트 제작에 시간이 많이 걸리지 않는 데에 반해 한글은 초성, 중성, 종성의 다양한 조합으로 인해 11,172자의 글자를 모두 디자인해야 합니다. 따라서 폰트 제작에 수많은 시간과 비용이 들게 되고 결국 한글 폰트의 부족이라는 결과로 이어지게 됩니다.
- 
+
 그래서 한글 폰트 제작에 도움이 되는 프로그램을 만들고자 해당 프로젝트를 시작하게 되었습니다.
 
 # Used Tools
@@ -21,11 +21,13 @@
 - 해당 프로젝트를 `git clone https://github.com/bamcasa/new_A.Ifonts.git` 또는 압축 파일 다운로드를 통해서 다운받습니다.
 
 - 프로젝트의 루트 폴더에서 다음을 실행하여 파이썬 라이브러리를 설치합니다.
+
 ```sh
 pip install -r requirements.txt
 ```
 
 - `result` 폴더로 이동 후 다음을 실행하여 Node.js 라이브러리를 설치합니다.
+
 ```sh
 npm install
 ```
@@ -58,7 +60,6 @@ python division.py
 
 - 티처블 머신으로 자음과 모음을 학습시킨 모델을 이용하여 추출된 음운을 분류합니다.
 
-
 ### Formulate
 
 ```sh
@@ -83,6 +84,24 @@ python covent_name.py
 
 - 이미지들을 `glyph` 폴더에 초성, 중성, 종성으로 분류하여 저장합니다.
 
+### Make Transparent
+
+```sh
+python invisible.py
+```
+
+- 합성에 용이하도록 이미지들을 투명하게 만듭니다.
+
+### Add Some Mo
+
+```sh
+python dual.py
+```
+
+- 모음을 결합하여 이중모음을 만듭니다.
+
+(여기서 이중모음은 모델에 학습되지 않은, 단순히 모음 두개가 합쳐진 모양의 이중모음을 뜻합니다.)
+
 ### Combine
 
 ```sh
@@ -98,7 +117,3 @@ node ttf.js
 ```
 
 - `.png` 파일들을 `.ttf` 파일로 만들고 `fonts` 폴더에서 만들어진 폰트를 확인합니다.
-
-# 메모장..
-
-[손글씨 데이터셋](https://drive.google.com/file/d/1dIlFuKEJLK09IqLET5nfai3q7h_XAOmT/view?usp=sharing)
--- result/combine.py
+++ result/combine.py
@@ -20,7 +20,6 @@ y축
 """
 
 base = cv2.imread("combine_base.png")  # 224 * 244
-dual_base = cv2.imread("dual_base.png")  # 224 * 244
 
 
 def double(mo_path, mo_start, ja_start):
@@ -132,81 +131,6 @@ def triple(mo_path, mo_start, ja1_start, ja2_start):
             base[:, :] = 255  # 베이스 초기화
 
 
-def make_dual_mo(mo_unicode_str, mo1_start, mo2_start):
-    '''이중모음 생성'''
-
-    # 이중모음 한글
-    mo_name = bytes(
-        "\\u" + mo_unicode_str.lower(), "utf8").decode("unicode_escape")
-
-    # 이중모음에 따른 합성 대상 모음 지정
-    if mo_name == "ㅒ":
-        mo1_name = "ㅑ"
-        mo2_name = "ㅣ"
-    elif mo_name == "ㅐ":
-        mo1_name = "ㅏ"
-        mo2_name = "ㅣ"
-    elif mo_name == "ㅞ":
-        mo1_name = "ㅜ"
-        mo2_name = "ㅔ"
-    elif mo_name == "ㅘ":
-        mo1_name = "ㅗ"
-        mo2_name = "ㅏ"
-    elif mo_name == "ㅙ":
-        mo1_name = "ㅗ"
-        mo2_name = "ㅐ"
-    elif mo_name == "ㅝ":
-        mo1_name = "ㅜ"
-        mo2_name = "ㅓ"
-    elif mo_name == "ㅚ":
-        mo1_name = "ㅗ"
-        mo2_name = "ㅣ"
-    elif mo_name == "ㅖ":
-        mo1_name = "ㅕ"
-        mo2_name = "ㅣ"
-    elif mo_name == "ㅢ":
-        mo1_name = "ㅡ"
-        mo2_name = "ㅣ"
-    elif mo_name == "ㅔ":
-        mo1_name = "ㅓ"
-        mo2_name = "ㅣ"
-    elif mo_name == "ㅟ":
-        mo1_name = "ㅜ"
-        mo2_name = "ㅣ"
-
-    mo1_unicode_str = str(mo1_name.encode("unicode_escape")).replace(
-        "b'\\\\u", "").replace("'", "").upper()
-    mo2_unicode_str = str(mo2_name.encode("unicode_escape")).replace(
-        "b'\\\\u", "").replace("'", "").upper()
-
-    mo1_path = f"glyph/mo/{mo1_unicode_str}.png"
-    mo2_path = f"glyph/mo/{mo2_unicode_str}.png"
-
-    mo1 = cv2.imread(mo1_path)  # 모음 1 불러오기
-    mo1_height, mo1_width, _ = mo1.shape  # 모음 1 픽셀 정보 저장
-    mo2 = cv2.imread(mo2_path)  # 모음 2 불러오기
-    mo2_height, mo2_width, _ = mo2.shape  # 모음 2 픽셀 정보 저장
-
-    # 모음 합성 시작점 설정
-    mo1_height_start = mo1_start[0]
-    mo1_width_start = mo1_start[1]
-    mo2_height_start = mo2_start[0]
-    mo2_width_start = mo2_start[1]
-
-    dual_base[mo1_height_start:mo1_height_start+mo1_height,
-              mo1_width_start:mo1_width_start+mo1_width] = mo1  # 모음 1 합성
-
-    dual_base[mo2_height_start:mo2_height_start+mo2_height,
-              mo2_width_start:mo2_width_start+mo2_width] = mo2  # 모음 2 합성
-
-    mo_unicode_str = str(mo_name.encode("unicode_escape")).replace(
-        "b'\\\\u", "").replace("'", "").upper()  # 완성된 이중모음의 유니코드 값 (4자리)
-
-    cv2.imwrite(f"glyph/mo/{mo_unicode_str}.png", dual_base)
-
-    dual_base[:, :] = 255  # 베이스 초기화
-
-
 def get_unicode_int(ja1_name, mo_name, ja2_name=""):
     '''글자의 유니코드 정수값 생성'''
     ja1_table = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ",
@@ -225,42 +149,6 @@ def get_unicode_int(ja1_name, mo_name, ja2_name=""):
     unicode_int = (ja1_index*588 + mo_index*28 + ja2_index) + 44032
     return unicode_int
 
-
-# make_dual_mo(mo_unicode_str="3152",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅒ
-
-# make_dual_mo(mo_unicode_str="315F",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅟ
-
-# make_dual_mo(mo_unicode_str="3150",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅐ
-
-# make_dual_mo(mo_unicode_str="3158",
-#              mo1_start=(110, 60), mo2_start=(90, 120))  # ㅘ
-
-# make_dual_mo(mo_unicode_str="3154",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅔ
-
-# make_dual_mo(mo_unicode_str="315D",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅝ
-
-# make_dual_mo(mo_unicode_str="3156",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅖ
-
-# make_dual_mo(mo_unicode_str="315A",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅚ
-
-# make_dual_mo(mo_unicode_str="3162",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅢ
-
-# make_dual_mo(mo_unicode_str="3159",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅙ
-
-# make_dual_mo(mo_unicode_str="315E",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅞ
-
-
-''''''
 
 if not os.path.exists("save_dir"):
     os.mkdir("save_dir")
