c0182b9a51958976fe29dec326dea65b22bff700
Bokwang0310 <chdl2353@naver.com>
2020-11-22 00:53:09 +0900

    Imp making dual & Apply new model & Add sample
    상훈이가 커밋 못한 crop.py 변경 내용 수정, dual.py 완성, 새로운 모델
    적용

--- result/dual.py
+++ result/dual.py
@@ -2,12 +2,24 @@ import os
 import cv2
 import numpy as np
 
-
 """
 모음을 결합하여 이중모음 만듦
 """
 
 dual_base = cv2.imread("dual_base.png")  # 224 * 244
+
+
+def combine(roi, mo):
+    mo_gray = cv2.cvtColor(mo, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(mo_gray, 10, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+
+    bg = cv2.bitwise_and(roi, roi, mask=mask)
+    fg = cv2.bitwise_and(mo, mo, mask=mask_inv)
+
+    dist = cv2.add(bg, fg)
+
+    return dist
 
 
 def make_dual_mo(mo_unicode_str, mo1_start, mo2_start):
@@ -71,8 +83,16 @@ def make_dual_mo(mo_unicode_str, mo1_start, mo2_start):
     mo2_height_start = mo2_start[0]
     mo2_width_start = mo2_start[1]
 
+    mo1_roi = dual_base[mo1_height_start:mo1_height_start+mo1_height,
+                        mo1_width_start:mo1_width_start+mo1_width]  # 모음 1 합성 영역 지정
+    mo1 = combine(mo1_roi, mo1)
+
     dual_base[mo1_height_start:mo1_height_start+mo1_height,
               mo1_width_start:mo1_width_start+mo1_width] = mo1  # 모음 1 합성
+
+    mo2_roi = dual_base[mo2_height_start:mo2_height_start+mo2_height,
+                        mo2_width_start:mo2_width_start+mo2_width]  # 모음 2 합성 영역 지정
+    mo2 = combine(mo2_roi, mo2)
 
     dual_base[mo2_height_start:mo2_height_start+mo2_height,
               mo2_width_start:mo2_width_start+mo2_width] = mo2  # 모음 2 합성
@@ -84,36 +104,38 @@ def make_dual_mo(mo_unicode_str, mo1_start, mo2_start):
 
     dual_base[:, :] = 255  # 베이스 초기화
 
+    print(mo1_name, mo2_name)
 
-# make_dual_mo(mo_unicode_str="3152",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅒ
 
-# make_dual_mo(mo_unicode_str="315F",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅟ
+make_dual_mo(mo_unicode_str="3152",
+             mo1_start=(70, 105), mo2_start=(70, 120))  # ㅒ
 
-# make_dual_mo(mo_unicode_str="3150",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅐ
+make_dual_mo(mo_unicode_str="315F",
+             mo1_start=(100, 70), mo2_start=(90, 110))  # ㅟ
+
+make_dual_mo(mo_unicode_str="3150",
+             mo1_start=(70, 95), mo2_start=(70, 120))  # ㅐ
 
 make_dual_mo(mo_unicode_str="3158",
              mo1_start=(110, 60), mo2_start=(90, 120))  # ㅘ
 
-# make_dual_mo(mo_unicode_str="3154",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅔ
+make_dual_mo(mo_unicode_str="3154",
+             mo1_start=(75, 90), mo2_start=(70, 130))  # ㅔ
 
-# make_dual_mo(mo_unicode_str="315D",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅝ
+make_dual_mo(mo_unicode_str="315D",
+             mo1_start=(100, 60), mo2_start=(100, 100))  # ㅝ
 
-# make_dual_mo(mo_unicode_str="3156",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅖ
+make_dual_mo(mo_unicode_str="3156",
+             mo1_start=(75, 80), mo2_start=(70, 120))  # ㅖ
 
-# make_dual_mo(mo_unicode_str="315A",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅚ
+make_dual_mo(mo_unicode_str="315A",
+             mo1_start=(95, 60), mo2_start=(100, 115))  # ㅚ
 
-# make_dual_mo(mo_unicode_str="3162",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅢ
+make_dual_mo(mo_unicode_str="3162",
+             mo1_start=(95, 60), mo2_start=(90, 115))  # ㅢ
 
-# make_dual_mo(mo_unicode_str="3159",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅙ
+make_dual_mo(mo_unicode_str="3159",
+             mo1_start=(75, 55), mo2_start=(0, 0))  # ㅙ
 
-# make_dual_mo(mo_unicode_str="315E",
-#              mo1_start=(75, 120), mo2_start=(80, 55))  # ㅞ
+make_dual_mo(mo_unicode_str="315E",
+             mo1_start=(75, 50), mo2_start=(0, 0))  # ㅞ
